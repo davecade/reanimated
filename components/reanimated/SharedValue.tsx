3f17d00e8862a8289f34182c903aebbd84482ea1
Dave Cadelina <dave.cadelina@reckon.com>
2024-11-10 19:46:20 +1100

    using shared valursd

--- components/reanimated/SharedValue.tsx
+++ components/reanimated/SharedValue.tsx
@@ -1,25 +1,45 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import Animated, { useSharedValue } from "react-native-reanimated";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 
+// Using Shared Value
 const SharedValue = () => {
+  // Set the default values
   const width = useSharedValue(100);
   const height = useSharedValue(100);
+  const backgroundColor = useSharedValue("red");
 
+  // When this runs, it will change the values of the shared values
   const onAnimate = () => {
     width.value = Math.random() * 300;
     height.value = Math.random() * 300;
+    backgroundColor.value =
+      backgroundColor.value === "red"
+        ? "green"
+        : backgroundColor.value === "green"
+        ? "blue"
+        : backgroundColor.value === "blue"
+        ? "orange"
+        : backgroundColor.value === "orange"
+        ? "purple"
+        : "red";
   };
+
+  // Created an animated style to apply to the animated view
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      width: width.value,
+      height: height.value,
+      backgroundColor: backgroundColor.value,
+    };
+  });
 
   return (
     <View>
-      <Animated.View
-        style={{
-          width: width,
-          height: height,
-          backgroundColor: "blue",
-        }}
-      ></Animated.View>
+      <Animated.View style={[animatedStyles]}></Animated.View>
       <Button title="Animate" onPress={onAnimate} />
     </View>
   );
